3e3bef293c66b809fbe66b1868a711ca605ee0fc
sanskrutikukadiya3303 <165458766+sanskrutikukadiya3303@users.noreply.github.com>
2025-08-22 12:37:55 +0530

    Update model.py

--- codes/model.py
+++ codes/model.py
@@ -1,3 +1,12 @@
 from efficientnet_pytorch import EfficientNet
 import torch
 import torch.nn as nn
+def init_model(model_name, train=True, trn_layers=2):
+    '''
+    Initialize the model
+    '''
+    # Load pre-trained model
+    model = EfficientNet.from_pretrained('efficientnet-b4', num_classes=5)
+
+    if train:
+        # Load model state dict for training mode
