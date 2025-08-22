4c1a1341dbc695cba2766dc64df7ffc1f8dd7710
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2025-08-22 12:38:11 +0530

    Update preprocessing.py
    define need func

--- codes/preprocessing.py
+++ codes/preprocessing.py
@@ -2,3 +2,11 @@ import numpy as np
 import torch
 import cv2
 import random
+
+
+def prepare_image():
+def crop_black():
+def circle_crop():
+def crop_for_squares():
+def standard_crop():
+def random_crop():
