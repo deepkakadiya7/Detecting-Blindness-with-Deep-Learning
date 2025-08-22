f5094e71059f17eb49f4d7d1e44967007cd1db1f
sanskrutikukadiya3303 <165458766+sanskrutikukadiya3303@users.noreply.github.com>
2025-08-22 12:38:15 +0530

    Update model.py

--- codes/model.py
+++ codes/model.py
@@ -10,3 +10,9 @@ def init_model(model_name, train=True, trn_layers=2):
 
     if train:
         # Load model state dict for training mode
+      model.load_state_dict(torch.load(f'../models/model_{model_name}.bin', map_location=torch.device('cpu')))
+        
+        # Freeze first layers
+        for child in list(model.children())[:-trn_layers]:
+            for param in child.parameters():
+                param.requires_grad = False
