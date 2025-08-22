d4c30fbd163f92080e90ae2fc0512b82b79ca649
sanskrutikukadiya3303 <165458766+sanskrutikukadiya3303@users.noreply.github.com>
2025-08-22 12:34:17 +0530

    Update data.py

--- codes/data.py
+++ codes/data.py
@@ -39,3 +39,14 @@ class EyeData(Dataset):
             image = self.transform(image)  # Apply transformations if any
         label = torch.tensor(self.data.loc[idx, 'diagnosis'])  # Get label
         return {'image': image, 'label': label}
+        class EyeTestData(EyeData):
+    def __getitem__(self, idx):
+        img_id = str(self.data.loc[idx, 'id_code']).strip()  # Get image ID
+        img_name = find_image_file(self.directory, img_id)  # Find image file
+        image = prepare_image(img_name, self.image_size, do_random_crop=False)  # Preprocess the image
+        if self.transform is not None:
+            image = self.transform(image)  # Apply transformations if any
+        # For test data, if no label exists, you may want to handle it differently
+        label = torch.tensor(self.data.loc[idx, 'diagnosis'])  # Get label
+        return {'image': image, 'label': label}
+
